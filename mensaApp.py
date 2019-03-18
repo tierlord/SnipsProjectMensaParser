bd12893e7ed1fe37634fe9ef1a90080482839b95
tierlord <xtra-j@web.de>
2019-03-18 22:52:20 +0100

    fix

--- mensaApp.py
+++ mensaApp.py
@@ -1,32 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import datetime, time
 
-import configparser
-from hermes_python.hermes import Hermes
-from hermes_python.ffi.utils import MqttOptions
-from hermes_python.ontology import *
-import io
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+try:
+    driver = webdriver.Chrome(options=chrome_options)
+except:
+    print("Could not create driver. Maybe you have the wrong Chrome version.")
+    exit()
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
+driver.get("https://www.my-stuwe.de/mensa/mensa-reutlingen/")
 
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
-        return dict()
-
-def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+gerichte = ""
 
 def removeIngrHint(gericht):
     klammer_auf = gericht.find('[')
@@ -116,20 +103,5 @@ def chooseDay(request):
     global gerichte
     gerichte += "Folgende Gerichte gibt es " + request + " in der Mensa:\n"
     getMeals()
-
-def action_wrapper(hermes, intentMessage, conf):
-    """ Write the body of the function that will be executed once the intent is recognized. 
-    In your scope, you have the following objects : 
-    - intentMessage : an object that represents the recognized intent
-    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined. 
-      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName'] 
-     
-    Refer to the documentation for further details. 
-    """ 
-    import mensaApp
-
-    gerichte = mensaApp.chooseDay(intentMessage.slots.tag.first().value)
-
-    current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, text=gerichte)
+    driver.close()
+    return gerichte

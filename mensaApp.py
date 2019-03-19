ef89a988b236eb91e66179366b9a71df5568b4ae
tierlord <xtra-j@web.de>
2019-03-19 12:31:25 +0100

    Update mensaApp.py

--- mensaApp.py
+++ mensaApp.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.options import Options
 import datetime, time
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+options = Options()
+options.add_argument("--headless")
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Firefox(options=options)
 
 driver.get("https://www.my-stuwe.de/mensa/mensa-reutlingen/")
 

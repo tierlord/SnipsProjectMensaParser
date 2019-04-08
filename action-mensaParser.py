4e17fd64da6fe46cd48deb2929281cff6b6a25af
tierlord <xtra-j@web.de>
2019-04-08 16:42:13 +0200

    Update action-mensaParser.py

--- action-mensaParser.py
+++ action-mensaParser.py
@@ -63,5 +63,5 @@ def subscribe_intent_callback(hermes, intent_message):
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h\
-            .subscribe_intents(subscribe_intent_callback)
+            .subscribe_intents(subscribe_intent_callback)\
             .start()

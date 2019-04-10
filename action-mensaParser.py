960a097251ee967dc860d4984dba4172a21ca51b
tierlord <xtra-j@web.de>
2019-04-10 12:34:06 +0200

    Update action-mensaParser.py

--- action-mensaParser.py
+++ action-mensaParser.py
@@ -55,5 +55,4 @@ def gerichtWaehlen (hermes, message):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("tierlord:WasGibts", gerichteVorlesen)
-        h.start()
+        h.subscribe_intent("tierlord:WasGibts", gerichteVorlesen).start()

5da42d3a5cb3a90ec1f7bda07658a31c18bb6b5c
tierlord <xtra-j@web.de>
2019-05-09 16:55:32 +0200

    Update action-mensaParser.py

--- action-mensaParser.py
+++ action-mensaParser.py
@@ -39,8 +39,7 @@ def receive_meals(hermes, message, day, menu):
                 meals_string += " möchtest du das bestellen?"
                 global gericht_gewaehlt
                 gericht_gewaehlt = menu
-                dialogue_conf = DialogueConfiguration() \
-                .enable_intent("tierlord:Bestaetigen")
+                dialogue_conf = DialogueConfiguration().enable_intent("tierlord:Bestaetigen")
                 return hermes.publish_continue_session(message.session_id, meals_string, ["tierlord:Bestaetigen"])
             meals_string += " was möchtest du bestellen?"
             return hermes.publish_continue_session(message.session_id, meals_string, ["tierlord:Waehlen"])
@@ -81,8 +80,7 @@ def gerichtBestaetigen (hermes, message):
     msg = "Alles klar. Ich habe " + gericht_gewaehlt + " für dich bestellt."
     global client
     client.publish("menu/bestellung", gericht_gewaehlt, retain=True)
-    dialogue_conf = DialogueConfiguration() \
-    .disable_intent("tierlord:Bestaetigen") \
+    dialogue_conf = DialogueConfiguration().disable_intent("tierlord:Bestaetigen")
     return hermes.publish_end_session(message.session_id, msg)
 
 

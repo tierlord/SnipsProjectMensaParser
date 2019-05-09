7ea098972a323786ac2610412ffafe92ee5c7471
tierlord <xtra-j@web.de>
2019-05-09 16:47:58 +0200

    Update action-mensaParser.py

--- action-mensaParser.py
+++ action-mensaParser.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from hermes_python.hermes import Hermes
+from hermes_python.ontology.dialogue.session import DialogueConfiguration
 import paho.mqtt.client as mqtt
 import json, time
 from threading import Thread
 
 meals_json = None
+gericht_gewaehlt = None
+client = None
 
 def parse_meals(meals, day, menu):
     msg = ""
@@ -32,11 +35,20 @@ def receive_meals(hermes, message, day, menu):
     for i in range (5):
         if meals_json:
             meals_string = parse_meals(meals_json, day, menu)
+            if day and menu:
+                meals_string += " möchtest du das bestellen?"
+                global gericht_gewaehlt
+                gericht_gewaehlt = menu
+                dialogue_conf = DialogueConfiguration() \
+                .enable_intent("tierlord:Bestaetigen") \
+                return hermes.publish_continue_session(message.session_id, meals_string, ["tierlord:Bestaetigen"])
+            meals_string += " was möchtest du bestellen?"
             return hermes.publish_continue_session(message.session_id, meals_string, ["tierlord:Waehlen"])
         time.sleep(1)
     return hermes.publish_end_session(message.session_id, "Es konnten keine Gerichte geladen werden.")
 
 def gerichteVorlesen (hermes, message):
+    global client
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
@@ -61,6 +73,16 @@ def gerichtWaehlen (hermes, message):
     if request == "Angebot":
         request = "das Angebot des Tages"
     msg = "Okay, ich habe " + request + " für dich bestellt."
+    global client
+    client.publish("menu/bestellung", request, retain=True)
+    return hermes.publish_end_session(message.session_id, msg)
+
+def gerichtBestaetigen (hermes, message):
+    msg = "Alles klar. Ich habe " + gericht_gewaehlt + " für dich bestellt."
+    global client
+    client.publish("menu/bestellung", gericht_gewaehlt, retain=True)
+    dialogue_conf = DialogueConfiguration() \
+    .disable_intent("tierlord:Bestaetigen") \
     return hermes.publish_end_session(message.session_id, msg)
 
 
@@ -68,4 +90,5 @@ with Hermes("localhost:1883") as h:
     h \
         .subscribe_intent("tierlord:WasGibts", gerichteVorlesen) \
         .subscribe_intent("tierlord:Waehlen", gerichtWaehlen) \
+        .subscribe_intent("tierlord:Bestaetigen", gerichtBestaetigen) \
         .start()
